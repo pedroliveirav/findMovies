a2db2d29f67d215446c921c5d33d4f4890cef10b
Pedro Oliveira <pedroliveirav1@gmail.com>
2021-12-25 23:35:11 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,68 @@
+// Swiper.js
+
+var swiper = new Swiper(".findMoviesSwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30
+});
+
+// Switch and Search
+
+const popularMovies = document.querySelectorAll('.findMovies-popular-movie');
+const movieTitle = document.querySelector('.findMovies-title');
+const movieScore = document.querySelector('.findMovies-score');
+const movieDescription = document.querySelector('.findMovies-description');
+const popularMoviesContent = document.querySelector('.findMovies-content');
+
+const getPopularMovies = () => {
+
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=76086750d951418bebc911d2566df20a&language=en-US&page=1").then(res => res.json()).then(res => {
+
+        for (let i = 0; i < res.results.length; i++) {
+            popularMovies[i].style.backgroundImage = 'url(https://image.tmdb.org/t/p/original' + res.results[i].poster_path + ')';
+
+            popularMovies[i].setAttribute("id", res.results[i].id)
+            
+        }
+
+    })
+    
+}
+
+const setTitle = (title) => {
+    movieTitle.innerText = title;
+}
+
+const setScore = (score) => {
+    movieScore.innerText = score;
+}
+
+const setDescription = (description) => {
+    movieDescription.innerText = description;
+}
+
+const getMovieById = (id) => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=76086750d951418bebc911d2566df20a&language=en-US`).then(res => res.json()).then(res => {
+
+        setTitle(res.title);
+        setScore(res.vote_average);
+        setDescription(res.overview);
+    })
+}
+
+const handleSelectMovie = (movieNumber) => {
+    const selectedMovieId = popularMovies[movieNumber-1].getAttribute("id");
+    getMovieById(selectedMovieId);
+}
+
+const searchByTitle = () => {
+    const movieTitleInput = document.querySelector('.findMovies-movie-title-input').value;
+    const movieImage = document.querySelector('.findMovies-seached-film-image');
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=76086750d951418bebc911d2566df20a&query=${movieTitleInput}`).then(res => res.json()).then(res => {
+        for (let i = 0; i < res.results.length; i++) {
+            popularMovies[i].style.backgroundImage = 'url(https://image.tmdb.org/t/p/original' + res.results[i].poster_path + ')';
+            popularMovies[i].setAttribute("id", res.results[i].id)
+            
+        }    
+    })
+}
